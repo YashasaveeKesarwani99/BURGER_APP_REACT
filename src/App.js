32e23b92d23b498f4fe95d553859ea04f8173f7a
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-09-29 11:53:32 +0530

    Created Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./styles.css";
-
+import Layout from "../Components/Layouts/layout";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Layout>
+      <p>test</p>
+    </Layout>
   );
 }
