4794e2c4f748accf94769d832b5f92a09c51388f
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-10-09 20:10:29 +0530

    Modal style updated

--- Components/UI/Modal/modal.js
+++ Components/UI/Modal/modal.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./modal.css";
 const Modal = (props) => {
-  return <div className="Modal">{props.children}</div>;
+  return (
+    <div
+      className="Modal"
+      style={{
+        transform: props.show ? "translateY(0)" : "trandslateY(-100vh)",
+        opacity: props ? "1" : "0"
+      }}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Modal;
